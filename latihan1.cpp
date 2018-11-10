7e0a30921be327db1656a25e0cb7feaaf85f9a61
nindahidayat <nindahidayat07@gmail.com>
2018-11-10 13:33:42 +0700

    praktikum5 latihan1

---
+++ latihan1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a,max=0;
+
+	do {
+		cout << "masukan bilangan :";
+	cin >> a;
+
+	if (a>max)
+		max=a;
+	}
+
+	while (a!=0);
+	cout << max;
+
+	return 0;
+
+	
+}
